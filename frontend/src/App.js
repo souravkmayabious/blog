454a637f3b084f14396692fb8f8c8776f17abef1
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-02-17 16:20:17 +0530

    cleanup react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          Learn React TEST BRANCH
-
-          Learn React TEST 2
-        </a>
-      </header>
+    <div className="container-fluid">
+      <button className='btn btn-success'>Test</button>
     </div>
   );
 }
