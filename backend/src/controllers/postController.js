b9e89d1c8eb9c5eb2b7ee183c975d33fbd9d0780
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-03-19 11:23:30 +0530

    update add , edit , delete post

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -1,21 +1,51 @@
 const  slugify = require('slugify');
 const  Post  = require("../models/postModel");
-const  User  = require("../models/userModel");
+//const  User  = require("../models/userModel");
+const PostCategories = require("../models/postCategoriesModel");
+const PostTags = require("../models/postTagsModel");
 
 //Add post
 const addPost = async (req, res) => {
-    const {title,body} = req.body;
-    if(!title || !body) return res.status(400).json({message: "Require all fields", success: false});
+    const {title,body,category,tag} = req.body;
+    if(!title || !body || !category || !tag) return res.status(400).json({message: "Require all fields : title, body, category, tag", success: false});
+
+    if (!Array.isArray(category) || category.length === 0) {
+        return res.status(400).json({ message: "Category must be a non-empty array", success: false });
+    }
+
+    if (!Array.isArray(tag) || tag.length === 0) {
+        return res.status(400).json({ message: "Tag must be a non-empty array", success: false });
+    }
+
     try{
         const slug = slugify(title, '_');
         const post = await Post.create({
             title,
             user_id : req.account_id,
             slug,
-            body
+            body,
           });
         if (post) {
-            return res.status(200).json({message: "Post created success", success: true});
+            //insert in Post cateory
+            const categoryInserts = category.map(catId => {
+                return PostCategories.create({
+                    post_id: post.post_id,
+                    category_id: catId,
+                });
+            });
+
+            //insert in Post category
+            const tagInserts = tag.map(tagId => {
+                return PostTags.create({
+                    post_id: post.post_id,
+                    tag_id: tagId,
+                });
+            });
+
+            // Wait for both category and tag insertions to finish
+            await Promise.all([...categoryInserts, ...tagInserts]);
+
+            return res.status(200).json({message: "Post created success", success: true, id:post.post_id});
         } else {
             return res.status(400).json({message: "Failed to create post", success: false});
         }
@@ -73,13 +103,53 @@ const viewMyPosts = async (req, res) => {
 
 //Edit post
 const editPost = async (req, res) => {
-    const {title,body} = req.body;
-    if(!title || !body) return res.status(400).json({message: "Require all fields", success: false});
+    const {title,body, category, tag} = req.body;
+    if (!title || !body || !category || !tag) {
+        return res.status(400).json({ message: "Require all fields: title, body, category, tag", success: false });
+    }
+
+    if (!Array.isArray(category) || category.length === 0) {
+        return res.status(400).json({ message: "Category must be a non-empty array", success: false });
+    }
+
+    if (!Array.isArray(tag) || tag.length === 0) {
+        return res.status(400).json({ message: "Tag must be a non-empty array", success: false });
+    }
+
     try{
         const post = await Post.findOne({where:{post_id: req.params.id,user_id:req.account_id}});
+        const postId = req.params.id;
         if(post){
             const slug = slugify(title, '_');
             await post.update({ title , slug , body });
+
+            // post.title = title;
+            // post.body = body;    
+            // await post.save();
+
+            // Remove old associations
+            await PostCategories.destroy({ where: { post_id: postId } });
+            await PostTags.destroy({ where: { post_id: postId } });
+
+             // Add new category associations
+            const categoryInserts = category.map(catId => {
+                return PostCategories.create({
+                    post_id: post.post_id,
+                    category_id: catId,
+                });
+            });
+
+            // Add new tag associations
+            const tagInserts = tag.map(tagId => {
+                return PostTags.create({
+                    post_id: post.post_id,
+                    tag_id: tagId,
+                });
+            });
+
+            // Wait for both category and tag insertions to finish
+            await Promise.all([...categoryInserts, ...tagInserts]);
+
             return res.status(200).json({message: "Post edit success", success: true});
         }else{
             return res.status(400).json({message: "No post found / Unauthorised", success: false});
@@ -118,6 +188,10 @@ const deletePost = async (req, res) => {
     try{
         const post = await Post.findOne({where:{post_id: req.params.id,user_id:req.account_id}});
         if(post){
+             // Remove associated categories and tags
+            await PostCategories.destroy({ where: { post_id: req.params.id } });
+            await PostTags.destroy({ where: { post_id: req.params.id } });
+
             await post.destroy();
             return res.status(200).json({message: "Post delete success", success: true});
         }else{
